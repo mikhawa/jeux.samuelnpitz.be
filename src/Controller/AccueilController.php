56f786ec98aae2cf7fb135c16c09916ac50698f9
mikhawa <michaeljpitz@gmail.com>
2024-03-09 15:42:01 +0100

    récupération des jeux

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+# Entité Article
+use App\Entity\Article;
+# Interface de notre ORM
+use Doctrine\ORM\EntityManagerInterface;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        # Récupération de la table Article
+        $articleRepository = $entityManager->getRepository(Article::class)->findAll();
+
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'articles' => $articleRepository,
         ]);
     }
 }
